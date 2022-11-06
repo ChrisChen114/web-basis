567dd97b209edc5f289d772cf97ff32ddb335b36
qchen31 <qchen31@nyit.edu>
2022-11-05 22:10:53 -0700

    ✨ feat(push-replace): add push/replace
    add push/replace mode

---
+++ ReactFamily/react_staging_done/19_react_staging_route_push-replace/src/pages/Home/Message/Message.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import {Link,Route,Routes} from 'react-router-dom'
+import {Detail} from './Detail/Detail';
+
+export default class Message extends Component {
+  state = {
+    messageArr:[
+      {id:'01',title:'消息1'},
+      {id:'02',title:'消息2'},
+      {id:'03',title:'消息3'},
+    ]
+  }
+  render() {
+    const {messageArr} = this.state;
+    return (
+      <div>
+        <ul>
+          {
+            messageArr.map((msgObj)=>{
+              return(
+                <li key={msgObj.id}>
+                  {/* S1, 向路由组件传递params参数 */}
+                  {/* <Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link>&nbsp;&nbsp; */}
+
+                  {/* S1, 向路由组件传递search参数 */}
+                  {/* <Link to={`/home/message/detail/?id=${msgObj.id}&title=${msgObj.title}`}>{msgObj.title}</Link>&nbsp;&nbsp; */}
+
+                  {/* S1, 向路由组件传递state参数 */}
+                  {/* 此处的state与组件中的state没有任何关系. */}
+                  {/* 这里要求写一个对象， state={}这样还不是对象，这只是js表达式；对象需要这样写-to={{}} */}
+                  <Link replace to='/home/message/detail' state={{id:msgObj.id,title:msgObj.title}}>{msgObj.title}</Link>&nbsp;&nbsp;
+                </li>
+              )
+            })
+          }
+        </ul>
+        <hr />
+        {/* 注册路由  */}
+        <Routes>
+          {/* S2, 声明接收params参数 */}
+          {/* <Route path="detail/:id/:title" element={<Detail/>}/> */}
+
+          {/* S2, search参数无需声明接收，正常注册路由即可 */}
+          {/* <Route path="detail" element={<Detail/>}/> */}
+
+          {/* S2, state参数无需声明接收，正常注册路由即可 */}
+          <Route path="detail" element={<Detail/>}/>
+        </Routes>
+      </div>
+    )
+  }
+}
