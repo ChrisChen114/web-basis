99ee401cae3a9497d6509ebb3d345644c3289ed1
qchen31 <qchen31@nyit.edu>
2022-11-07 21:05:13 -0800

    ✨ feat(react-redux): add Person constant
    add Person constant

---
+++ ReactFamily/react-redux_redo/src/redux/constant.js
@@ -0,0 +1,12 @@
+/*
+    保存常量信息，提供给reducers和actions使用
+*/
+
+// ××××××××××××××××××××××××××××
+// 以下是错误的写法  没有写export
+// export const INCREMENT = 'increment'
+export const INCREMENT = 'increment'
+export const DECREMENT = 'decrement'
+
+// Person容器组件用
+export const ADD_PERSON = 'add_person'
