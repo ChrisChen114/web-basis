9f2fee46a9a74cec34a8346e60246a8b47553504
qchen31 <qchen31@nyit.edu>
2022-11-07 21:07:13 -0800

    ✨ feat(react-redux): add Person container
    add Person component and container

--- ReactFamily/react-redux_redo/src/containers/Person/Person.jsx
+++ ReactFamily/react-redux_redo/src/containers/Person/Person.jsx
@@ -1,18 +1,46 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import { nanoid } from 'nanoid'
+import {addPerson} from '../../redux/actions/person.js'
 
-export default class Person extends Component {
+class Person extends Component {
+  // 添加一个人
+  addPerson = ()=>{
+    // 获取name和age
+    const name = this.nameNode.value;
+    const age = this.ageNode.value*1;
+    // 调用redux的
+    const personObj = {id:nanoid(),name,age}
+    this.props.addPerson(personObj)
+    this.nameNode.value = '';
+    this.ageNode.value = '';
+  }
+
   render() {
     return (
       <div>
-        <h2>我是Person组件,Count组件的和为??</h2>
-        <input type="text" placeholder='输入名字'/>&nbsp;
-        <input type="text" placeholder='输入年龄'/>
+        <h2>我是Person组件,Count组件的和为{this.props.count}</h2>
+        <input ref={c=>this.nameNode = c} type="text" placeholder='输入名字'/>&nbsp;
+        <input ref={c=>this.ageNode = c} type="text" placeholder='输入年龄'/>&nbsp;
+        <button onClick={this.addPerson}>添加</button>
         <ul>
-            <li>tom1--age1</li>
-            <li>tom2--age2</li>
-            <li>tom3--age3</li>
+          {
+            this.props.persons.map((personObj)=>{
+                return <li key={personObj.id}>{personObj.name}--{personObj.age}</li>
+            })
+          }
         </ul>
       </div>
     )
   }
 }
+
+export default connect(
+  state=>({
+        count:state.count,
+        persons:state.persons,
+  }),
+  {
+    addPerson,
+  }
+)(Person)
