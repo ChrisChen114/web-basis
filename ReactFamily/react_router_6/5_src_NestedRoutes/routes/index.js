481964386c97ace67c31aa28634b43f89edebb5e
qchen31 <qchen31@nyit.edu>
2022-11-08 15:15:34 -0800

    ✨ feat(Nested Routes): add Nested Routes
    add Nested Routes

---
+++ ReactFamily/react_router_6/5_src_NestedRoutes/routes/index.js
@@ -0,0 +1,33 @@
+import { Navigate } from 'react-router-dom'
+
+import About from "../pages/About"
+import Home from "../pages/Home"
+import Message from '../pages/Message'
+import News from '../pages/News'
+
+
+export default [
+  {
+    path: '/about',
+    element: <About />
+  },
+  {
+    path: '/home',
+    element: <Home />,
+    // 子路由children
+    children: [
+      {
+        path: 'news',
+        element: <News />
+      },
+      {
+        path: 'message',
+        element: <Message />
+      },
+    ]
+  },
+  {
+    path: '/',
+    element: <Navigate to='/about' />
+  },
+]
