0aa4dcf348b7e2a70d8a62958a66128c97a6eaf7
qchen31 <qchen31@nyit.edu>
2022-11-07 18:54:51 -0800

    🦄 refactor(react-redux):
    comment Provider meaning

--- ReactFamily/react-redux_redo/src/index.js
+++ ReactFamily/react-redux_redo/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store.js'
 
 const root = ReactDOM.render(
+  // import {Provider}，包裹App，让App所有后代容器组件都能接收到store
   <Provider store={store}>
     <React.StrictMode>
       <App />
