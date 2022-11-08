0ea2f9ffcdeaa2fa492e90d131b75a59e6c2caa2
qchen31 <qchen31@nyit.edu>
2022-11-08 14:52:01 -0800

    ✨ feat(useRoutes): add useRoutes
    add useRoutes

---
+++ ReactFamily/react_router_6/4_src_useRoutes_RouteTable/pages/Home.jsx
@@ -0,0 +1,12 @@
+// 函数里面用useState实现状态更新
+import React,{useState} from 'react'
+
+// Navigate只要渲染就会引起视图的切换
+
+export default function Home() {
+  return (
+    <div>
+        <h3>我是Home的内容</h3>
+    </div>
+  )
+}
