f0d26eb57743aeaa5822f9374b074df97f1f5571
qchen31 <qchen31@nyit.edu>
2022-11-08 16:57:22 -0800

    ✨ feat(Programmatic routing navigation): add useNavigate
    add useNavigate to finish Programmatic routing navigation

---
+++ ReactFamily/react_router_6/9_src_ProgRouteNavig/App.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import {Link,NavLink,Routes,Route,Navigate,useRoutes} from 'react-router-dom'
+import Header from './components/Header'
+// import About from './pages/About'
+// import Home from './pages/Home'
+import routes from './routes'
+
+export default function App() {
+  // 实现自定义高亮，放在这里写，是提高代码可读性，否则代码太长
+  // function computedClassName({isActive}){
+  //   return isActive?'list-group-item atguigu' : "list-group-item"
+  // }
+
+  // 路由表
+  // 根据路由表生成对应的路由规则
+  const element=useRoutes(routes)
+
+  return (
+    <div>
+      <div className="row">
+        <Header/>
+      </div>
+      <div className="row">
+        <div className="col-xs-2 col-xs-offset-2">
+          <div className="list-group">
+            {/* 路由链接 */}
+            {/* <NavLink className={ computedClassName} to="/about" >About</NavLink> */}
+            {/* <NavLink className={ computedClassName}  to="/home">Home</NavLink> */}
+            <NavLink className="list-group-item" to="/about" >About</NavLink>
+            {/* 如果不想让父级高亮，加'end'属性 */}
+            {/* <NavLink className="list-group-item" end to="/home">Home</NavLink> */}
+            <NavLink className="list-group-item" to="/home">Home</NavLink>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="panel">
+            <div className="panel-body">
+              {/* 注册路由 */}
+              {/* Routes-一个匹配上了，不再继续匹配 */}
+              {/* <Routes>
+                <Route path='/about' element={<About/>}></Route>
+                <Route path='/home' element={<Home/>}></Route>
+                <Route path='/' element={<Navigate to="/about"/>}></Route>
+              </Routes> */}
+              {/* 路由表的使用 */}
+              {element}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
